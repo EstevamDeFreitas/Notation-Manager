370a25fafc49e698efd39c1e1ea118f450955397
David <davidsantosrosa0@gmail.com>
2023-04-14 23:54:44 -0300

    Criado função adicionar

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const addBotao = document.getElementById('add-notation');
 console.log("Chegou aqui");
+
+var novaanotacao = window.document.querySelector('input#nova-anotacao')
+
+function adicionar() {
+    let na = String(novaanotacao.value)
+    const listagem = document.getElementById('anotation-list');
+    const elementoDivListagem = document.createElement('div');
+    elementoDivListagem.innerHTML = `${na}`;
+    elementoDivListagem.classList.add('list-group-item')
+    elementoDivListagem.classList.add('d-flex')
+    elementoDivListagem.classList.add('flex-column')
+
+    listagem.appendChild(elementoDivListagem);
+
+    novaanotacao.value = ''
+}
